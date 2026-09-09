d3b29ac8172103943c2c5eee5159832132fb1201
Maazin Osama Khattak <maazinkhattak74@gmail.com>
2026-09-09 10:14:38 -0600

    Time: 2 ms (99.37%), Space: 47.1 MB (59.12%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // hashmap to store the prev sums against their indexesd
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // loop through the array
+        for (int i = 0; i < nums.length; i++) {
+            // first check if the diff with target exists in the map
+            if (map.containsKey(target - nums[i])) return new int[]{map.get(target - nums[i]), i};
+            // if it is not contained we just add the current element and its index for later use
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
